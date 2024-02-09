63ad13f8920591b10c02b9c5ac2153e8d79a65d3
syed.wajahat <syed.wajahat@tpsonline.com>
2024-02-10 00:48:17 +0500

    BottomSheetView added

---
+++ ExternalPodTest/Classes/BottomSheetView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  PodSample
+//
+//  Created by Syed Wajahat Ali on 09/02/2024.
+//
+
+import SwiftUI
+
+struct BottomSheetView<Content: View>: View {
+    typealias Body = Never
+    var body: Never { fatalError() }
+
+    @Binding var isOpen: Bool
+
+    let maxHeight: CGFloat
+    let minHeight: CGFloat
+    let content: Content
+
+    init(isOpen: Binding<Bool>, maxHeight: CGFloat, ratio: CGFloat, @ViewBuilder content: () -> Content) {
+        self.minHeight = maxHeight * ratio
+        self.maxHeight = maxHeight
+        self.content = content()
+        self._isOpen = isOpen
+    }
+}
