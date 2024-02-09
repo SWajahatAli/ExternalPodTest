bab4b56fc3d23f1c9bda4de1ca3cc0470fedad0c
syed.wajahat <syed.wajahat@tpsonline.com>
2024-02-09 22:01:22 +0500

    Initial commit

---
+++ ExternalPodTest/Classes/ContentView.swift
@@ -0,0 +1,30 @@
+//
+//  ContentView.swift
+//  PodSample
+//
+//  Created by Syed Wajahat Ali on 09/02/2024.
+//
+
+import SwiftUI
+
+struct FullScreenModalView: View {
+    @Environment(\.presentationMode) var presentationMode
+    
+    var body: some View {
+        VStack{
+            Text("Presented full screen from SDK Directly")
+                .foregroundStyle(.white)
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
+                .background(Color.blue)
+                .edgesIgnoringSafeArea(.all)
+                .onTapGesture {
+                    presentationMode.wrappedValue.dismiss()
+                }
+        }
+    }
+}
+
+
+#Preview {
+    FullScreenModalView()
+}
