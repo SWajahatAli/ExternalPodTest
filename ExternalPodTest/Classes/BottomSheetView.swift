b25c3bbc7b5f0a316589c7fb5fe82202dc8f5fff
syed.wajahat <syed.wajahat@tpsonline.com>
2024-02-10 18:04:55 +0500

    Updated the body

--- ExternalPodTest/Classes/BottomSheetView.swift
+++ ExternalPodTest/Classes/BottomSheetView.swift
@@ -8,15 +8,21 @@
 import SwiftUI
 
 public struct BottomSheetView<Content: View>: View {
-    public typealias Body = Never
-    public var body: Never { fatalError() }
-
     @Binding var isOpen: Bool
 
     let maxHeight: CGFloat
     let minHeight: CGFloat
     let content: Content
-
+    
+    public var body: some View {
+        content
+            .frame(maxWidth: .infinity, maxHeight: maxHeight)
+            .background(Color.white) // Adjust as needed
+            .cornerRadius(16) // Adjust as needed
+            .offset(y: isOpen ? 0 : maxHeight) // Adjust as needed
+            .animation(.default) // Adjust animation as needed
+    }
+    
     public init(isOpen: Binding<Bool>, maxHeight: CGFloat, ratio: CGFloat, @ViewBuilder content: () -> Content) {
         self.minHeight = maxHeight * ratio
         self.maxHeight = maxHeight
